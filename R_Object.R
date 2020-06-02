3e97d6ea526be3971d6eaa6fa9cd66ee6a3bf459
Mohammad Danish <mohammaddanish85@gmail.com>
2020-06-02 11:34:33 +0530

    Delete R_Object.R

--- R_Object.R
+++
@@ -1,7 +0,0 @@
-
-# Program : Creating Objects in R
-# Creating the objects and printing their values.
-
-my_object= 1:10         # In this code we are simply creating an object named as my_object. 
-                        # storing values from 1-10.
-my_object               # Selecting this line and run , you will get an output printing numbers from 1-10.
