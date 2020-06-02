7f736c1db73cc67cbbadca70c9bfb31c80e0594f
Mohammad Danish <mohammaddanish85@gmail.com>
2020-06-02 11:35:26 +0530

    Add files via upload
    Code describes the objects in R Script.

---
+++ R_Object.R
@@ -0,0 +1,10 @@
+# Author : Mohammad Danish (Assistant Professor)
+# Program Name : Creating Objects in R
+# Creating the objects and printing their values.
+
+my_object= 1:10         # In this code we are simply creating an object named as my_object. 
+                        # storing values from 1-10 as integers.
+my_object               # Selecting this line and run, you will get an output printing numbers from 1-10.
+                        # This object consists of a vector having length of 10.
+
+plot(my_object)         # Plot function will plot the data in a 2 dimensional graph which can be seen in plot field.
